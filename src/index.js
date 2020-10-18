d09574813e497a9f7ffe37a7d7bdba4ee62728c2
TatsianaKhadasevich <tatsyanakhodasevich@gmail.com>
2020-10-18 20:55:05 +0300

    feat: task solved

--- src/index.js
+++ src/index.js
@@ -38,7 +38,21 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result='';
+    let preResult=''
+    for(let i=0;i<=expr.length-1;i+=10){
+    		if(expr[i]=='*') continue; 
+        let morseNum = Number(expr.slice(i,i+10)).toString();        
+        
+        for(let n=0; n<morseNum.length-1; n+=2){
+            let str=(morseNum[n]+morseNum[n+1]=='10')? '.':'-';
+            preResult += str;   
+        }
+
+        result += ((expr[i-1]=='*')? ' ':'') + MORSE_TABLE[preResult];
+        preResult='';
+    }
+return result;
 }
 
 module.exports = {
